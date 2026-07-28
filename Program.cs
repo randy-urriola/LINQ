5997676fc7b33cb80a0e9e79e954abb1d64860cd
randy-urriola <randy.urriolah@gmail.com>
2026-07-28 14:26:37 -0500

    Probando LINQ y sus funciones básicas por primera vez

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string[] frutas = ["Sandia", "Fresa", "Mango", "Mango de azucar", "Mango tommy"];
+    var esMango = frutas.Where(f => f.StartsWith("Mango")).ToList();
+    esMango.ForEach(m => Console.WriteLine(m));
+  }
+}
