11a3daac4c778c954acb3b8d5454a290e3884bd7
randy-urriola <randy.urriolah@gmail.com>
2026-08-03 09:53:20 -0500

    Probando los operadores básicos con LINQ

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -11,9 +11,41 @@ public class LinqQueries
       this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true }); // lo convierte el json a list book
     }
   }
-  
+
   public IEnumerable<Book> TodaLaColeccion()
   {
     return librosCollection;
+  }
+
+  public IEnumerable<Book> LibrosDespuesDel2000()
+  {
+    // Extention method
+    // return librosCollection.Where(x => x.Publisheddate.Year > 2000);
+
+    // Query syntax
+    return from libro in librosCollection
+           where libro.Publisheddate.Year > 2000
+           select libro;
+  }
+
+  public IEnumerable<Book> LibrosConMasDe250PagConTituloInAction()
+  {
+    // Extention method
+    //return librosCollection.Where(x => x.PageCount > 250 && x.Title.Contains("in Action"));
+
+    // Query syntax
+    return from libro in librosCollection
+           where libro.PageCount > 250 && libro.Title.Contains("in Action")
+           select libro;
+  }
+
+  public bool TodosLosLibrosTienenStatus()
+  {
+    return librosCollection.All(x => x.Status != string.Empty);
+  }
+  
+  public bool LibrosPublicadosEn2005()
+  {
+    return librosCollection.Any(x => x.Publisheddate.Year == 2005);
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿LinqQueries queries = new LinqQueries();
 
-ImprimirValores(queries.TodaLaColeccion());
+// Toda la coleccion
+//ImprimirValores(queries.TodaLaColeccion());
+
+// Libros despues del 2000
+//ImprimirValores(queries.LibrosDespuesDel2000());
+
+// Libros con mas de 250 paginas y con titulo In Action
+// ImprimirValores(queries.LibrosConMasDe250PagConTituloInAction());
+
+// Todos los libros tienen status
+// Console.WriteLine("Todos los libros tienen status: {0}", queries.TodosLosLibrosTienenStatus());
+
+// Libros publicados en 2005
+Console.WriteLine("Algun libro publicado en 2005: {0}", queries.LibrosPublicadosEn2005());
 
 void ImprimirValores(IEnumerable<Book> listadelibros)
 {
